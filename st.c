a8ae7616189430ea01a39307393b9d1c3368a0e7
arjaz <arjaz@protonmail.com>
2019-11-21 00:14:30 +0200

    Change scroll to half of the screen
    Add change scroll patch file

--- st.c
+++ st.c
@@ -1083,7 +1083,7 @@ kscrolldown(const Arg* a)
 	int n = a->i;
 
 	if (n < 0)
-		n = term.row + n;
+		n = term.row / 2 + n;
 
 	if (n > term.scr)
 		n = term.scr;
@@ -1101,7 +1101,7 @@ kscrollup(const Arg* a)
 	int n = a->i;
 
 	if (n < 0)
-		n = term.row + n;
+		n = term.row / 2 + n;
 
 	if (term.scr <= HISTSIZE-n) {
 		term.scr += n;
